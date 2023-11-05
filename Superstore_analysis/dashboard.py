1f5d32dbe9d44eea4fbe05e6456edfc7501e523b
Amjadali168 <amjadali1682002@gmail.com>
2023-11-05 19:25:10 +0530

    Superstore analysis

--- Superstore_analysis/dashboard.py
+++ Superstore_analysis/dashboard.py
@@ -109,3 +109,14 @@ st.subheader('Time Series Analysis')
 linechart = pd.DataFrame(filtered_df.groupby(filtered_df["month_year"].dt.strftime("%y : %b"))["Sales"].sum()).reset_index()
 fig2 = px.line(linechart, x = "month_year",y="Sales",labels={"Sales":"Amount"},height=500, width=1000,template="gridon")
 st.plotly_chart(fig2, use_container_width=True)
+
+with st.expander("View Data of TimeSeries:"):
+    st.write(linechart.T.style.background_gradient(cmap="Blues"))
+    csv = linechart.to_csv(index=False).encode("utf-8")
+    st.download_button('Download Data', data=csv, file_name="TimeSeries.csv", mime='text/csv')
+
+# create a treem based on Region, category , sub-category
+st.subheader("Hierarchical view of sales using TreeMap")
+fig3 = px.treemap(filtered_df, path=["Region","Category","Sub-Category"],values="Sales", hover_data=["Sales"], color="Sub-Category")
+fig3.update_layout(width=800, height=650)
+st.plotly_chart(fig3, use_container_width=True)
